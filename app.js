77e9a053e060c2ed0178848f1fc5681a690698ba
Chreol TRIGNON <chreoltri@gmail.com>
2018-08-17 18:44:03 +0300

    add file app.js

---
+++ app.js
@@ -0,0 +1,39 @@
+// console.log('hey Chreol');
+// setTimeout(function(){
+//     console.log('5Seconds have been passed')
+// }, 5000);
+
+// let time = 0
+// setInterval( ()=>{
+// time += 3;
+// console.log(`${time} seconds have been passed`)
+// }, 3000)
+
+// let time = 0
+// let timer = setInterval( ()=>{
+// time += 3;
+// console.log(`${time} seconds have been passed`)
+// if (time == 30){
+//     clearInterval(timer)
+//     }
+// }, 3000)
+
+// let time = 0
+// let timer = setInterval( ()=>{
+// time += 3;
+// console.log(`${time} seconds have been passed`)
+// if (time == 30){
+//     clearInterval(timer)
+//     }
+// }, 3000)
+
+let time = 0
+let timer = setInterval( ()=>{
+time += 1;
+console.log(`${time} seconds have been passed`)
+if (time > 10){
+    clearInterval(timer)
+    }
+}, 1000)
+
+
